5b0f8a1dedaa2ba18e4e9baaefd8480fef6224cd
Mars Oakley <a.oakley4589@gmail.com>
2024-11-13 14:42:32 +0000

    added Model to Blog

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '5gjINphIGBGdPPJI9E5x31U7jFvzOX1r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-moakley45-djangoblogpro-iqbcdaod6oe.ws.codeinstitute-ide.net',
                 '.herokuapp.com']
@@ -90,6 +90,10 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators

29eb7e43041b59449475b0d251011b48cc17a96a
Mars Oakley <a.oakley4589@gmail.com>
2024-11-19 12:55:01 +0000

    enable serving of image files

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = '5gjINphIGBGdPPJI9E5x31U7jFvzOX1r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-moakley45-djangoblogpro-iqbcdaod6oe.ws.codeinstitute-ide.net',
                 '.herokuapp.com']
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
@@ -49,6 +50,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap5',
     'blog',
+    'cloudinary',
     'django_summernote',
     'about',
 ]
